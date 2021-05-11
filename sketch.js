bb15d7f927e3855e7ee5827450ee10948032c88e
BrahvimWhtHtJr <83908614+BrahvimWhtHtJr@users.noreply.github.com>
2021-05-11 08:22:02 +0530

    Centered Canvas...now done properly.

--- sketch.js
+++ sketch.js
@@ -112,7 +112,7 @@ function preload() {
 
 function setup() {
   load.remove();
-  createCanvas(400, 400).position(windowWidth / 2, windowHeight / 2); //Of course you do that!
+  createCanvas(400, 400).position(windowWidth / 2 - 200, windowHeight / 2 - 200); //Of course you do that!
   music.play(); // :D
   fill(0);
 
